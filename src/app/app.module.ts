53df3e5e4c668a003181210eafe5cc4238e8986d
abhishekdey <abhishekdey.sdm@gmail.com>
2022-03-23 12:35:51 +0530

    4 files modified, 17 added, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,33 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { StoreModule } from '@ngrx/store';
-import { counterReducer } from '../store/counter.reducer';
+import { counterReducer } from './state/counter.reducer';
 import { MyCounterComponent } from './my-counter/my-counter.component';
+import { booksReducer } from './state/books.reducer';
+import { collectionReducer } from './state/collection.reducer';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookCollectionComponent } from './book-list/book-collection.component';
+
+import { GoogleBooksService } from './book-list/books.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    StoreModule.forRoot({ count: counterReducer }),
+    StoreModule.forRoot({
+      count: counterReducer,
+      books: booksReducer,
+      collection: collectionReducer,
+    }),
   ],
-  declarations: [AppComponent, HelloComponent, MyCounterComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    MyCounterComponent,
+    BookListComponent,
+    BookCollectionComponent,
+  ],
   bootstrap: [AppComponent],
+  providers: [],
 })
 export class AppModule {}
