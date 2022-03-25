27e985f44b0d376df7a3761045497097ece0794a
abhishekdey <abhishekdey.sdm@gmail.com>
2022-03-25 13:17:55 +0530

    3 files modified

--- src/app/state/index.ts
+++ src/app/state/index.ts
@@ -1,6 +1,10 @@
 import { counterReducer } from './counter.reducer';
 import { ActionReducerMap } from '@ngrx/store';
+import { booksReducer } from './books.reducer';
+import { collectionReducer } from './collection.reducer';
 
 export const store: ActionReducerMap<any> = {
   counterReducer,
+  booksReducer,
+  collectionReducer,
 };
