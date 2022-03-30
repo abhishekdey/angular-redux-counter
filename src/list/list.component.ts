7df76ab4cd5152587cbd99bb86c4332d77eec0ca
abhishekdey <abhishekdey.sdm@gmail.com>
2022-03-30 12:21:16 +0530

    1 file modified

--- src/list/list.component.ts
+++ src/list/list.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit, VERSION } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectBookCollection } from '../app/state/books.selectors';
 @Component({
   selector: 'my-app22',
   template: `<div> Child component </div> <my-ap2 [o]="2">eeee</my-ap2>`,
   styleUrls: [],
 })
 export class ListComponent implements OnInit {
-  constructor() {
+  bookCollection$ = this.store.select(selectBookCollection);
+  constructor(private store: Store) {
     console.log('constructor ListComponent');
   }
 
