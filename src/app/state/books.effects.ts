122a61d27bdf8af15125f12e74719ad565827446
abhishekdey <abhishekdey.sdm@gmail.com>
2022-04-14 16:33:29 +0530

    1 file modified

--- src/app/state/books.effects.ts
+++ src/app/state/books.effects.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { mergeMap } from 'rxjs/operators';
+import { map, mergeMap } from 'rxjs/operators';
 
 @Injectable()
 export class BookEffects {
-  loadMovies$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType('[Movies Page] Load Movies'),
-      mergeMap(() =>
-        toPromise('https://jsonplaceholder.typicode.com/todos/1').then(
-          (data) => data
+  loadMovies$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType('[Movies Page] Load Movies'),
+        mergeMap(() =>
+          toPromise('https://jsonplaceholder.typicode.com/todos/39').then(
+            (movies) => ({
+              type: '[Movies API] Movies Loaded Success',
+              payload: movies,
+            })
+          )
         )
-      )
-    )
+      ),
+    {
+      dispatch: false,
+    }
   );
   constructor(private actions$: Actions) {}
 }
