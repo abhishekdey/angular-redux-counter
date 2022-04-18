ebb192bb9f0a29743a62c5fd5654822d35206864
abhishekdey <abhishekdey.sdm@gmail.com>
2022-04-18 12:57:53 +0530

    1 file modified

--- src/app/state/books.effects.ts
+++ src/app/state/books.effects.ts
@@ -23,23 +23,23 @@ export class BookEffects {
     }
   );
 
-  loadMovies2$ = createEffect(
-    () =>
-      this.actions$.pipe(
-        ofType('[Movies Page] Load Movies'),
-        mergeMap(() =>
-          ajax('https://jsonplaceholder.typicode.com/todo/59').pipe(
-            map((movies) => ({
-              type: '[Movies API] Movies Loaded Success',
-              payload: movies,
-            }))
-          )
-        )
-      ),
-    {
-      dispatch: false,
-    }
-  );
+  // loadMovies2$ = createEffect(
+  //   () =>
+  //     this.actions$.pipe(
+  //       ofType('[Movies Page] Load Movies'),
+  //       mergeMap(() =>
+  //         ajax('https://jsonplaceholder.typicode.com/todo/59').pipe(
+  //           map((movies) => ({
+  //             type: '[Movies API] Movies Loaded Success',
+  //             payload: movies,
+  //           }))
+  //         )
+  //       )
+  //     ),
+  //   {
+  //     dispatch: false,
+  //   }
+  // );
 
   constructor(private actions$: Actions) {}
 }
